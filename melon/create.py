b35fa48c076e13ec964a8d6bc128d774a879ef9c
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-02-10 10:17:17 -0600

    Create create.py

---
+++ melon/create.py
@@ -0,0 +1,17 @@
+import turtle
+
+def square():
+    t = turtle.Turtle()
+    s = 100
+    # drawing first side
+    t.forward(s) # Forward turtle by s units
+    t.left(90) # Turn turtle by 90 degree
+    # drawing second side
+    t.forward(s) # Forward turtle by s units
+    t.left(90) # Turn turtle by 90 degrees
+    # drawing third side
+    t.forward(s) # Forward turtle by s units
+    t.left(90) # Turn turtle by 90 degrees
+    # drawing fourth side
+    t.forward(s) # Forward turtle by s units
+    t.left(90) # Turn turtle by 90 degree

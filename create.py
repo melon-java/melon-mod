d7a7c84a31475332280d3d5132f9c282c81ab886
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-02-09 22:29:02 -0600

    Create create.py

---
+++ create.py
@@ -0,0 +1,17 @@
+import turtle
+
+def square():
+
+   s = "10"
+   # drawing first side
+   t.forward(s) # Forward turtle by s units
+   t.left(90) # Turn turtle by 90 degree
+   # drawing second side
+   t.forward(s) # Forward turtle by s units
+   t.left(90) # Turn turtle by 90 degree
+   # drawing third side
+   t.forward(s) # Forward turtle by s units
+   t.left(90) # Turn turtle by 90 degree
+   # drawing fourth side
+   t.forward(s) # Forward turtle by s units
+   t.left(90) # Turn turtle by 90 degree
